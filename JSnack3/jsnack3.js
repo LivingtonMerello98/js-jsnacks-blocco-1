37bb9e3b39767bafb19818ae693b32543ff215db
LivingtonMerello98 <livington.merello@gmail.com>
2024-05-03 12:41:52 +0200

    [upd] jsnack3

--- JSnack3/jsnack3.js
+++ JSnack3/jsnack3.js
@@ -1,1 +1,14 @@
 'use strict'
+
+let sum = 0;
+
+// Utilizza un loop per chiedere all'utente di inserire un numero 10 volte.
+for (let i = 0; i < 10; i++) {
+    // Chiedi all'utente di inserire un numero utilizzando prompt .
+    const num = parseInt(prompt("Inserisci un numero:"));
+
+    sum += num;
+}
+
+console.log("La somma di tutti i numeri inseriti è:", sum);
+alert("La somma di tutti i numeri inseriti è: " + sum);
